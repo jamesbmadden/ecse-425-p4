680fcba8af1163c6a0008cc9de3107941803ecf6
James Madden <jamesbmadden@gmail.com>
2026-04-14 14:50:41 -0400

    ✅ reading from instrmem tested!

--- instrmem.vhd
+++ instrmem.vhd
@@ -8,8 +8,10 @@ entity instrmem is
 	port (
 		-- instrmem has to take in an address and return a value
 		clk : in std_logic;
+		w : in std_logic;
 		addr : in std_logic_vector(31 downto 0);
-		instr : out std_logic_vector(31 downto 0);
+		w_data : in std_logic_vector(31 downto 0);
+		instr : out std_logic_vector(31 downto 0)
 	);
 end instrmem;
 
@@ -17,12 +19,13 @@ architecture behaviour of instrmem is
 
 	signal memread : std_logic := '0';
 	signal waitrequest : std_logic;
+	signal addrint : integer := 0;
 
 	component memory is
 		PORT (
 			clock: IN STD_LOGIC;
 			writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-			address: IN INTEGER RANGE 0 TO ram_size-1;
+			address: IN INTEGER RANGE 0 TO 32767;
 			memwrite: IN STD_LOGIC;
 			memread: IN STD_LOGIC;
 			readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
@@ -32,18 +35,22 @@ architecture behaviour of instrmem is
 	
 begin
 
+	addrint <= to_integer(unsigned(addr));
+
 	-- on clock cycle, ask for a new read. it will be done on the next clock cycle
 	process(clk)
 	begin
-		memread <= '1';
+		if rising_edge(clk) then
+			memread <= '1';
+		end if;
 	end process;
 
 	-- connect to the appropriate ports of a memory instance
 	MEM: memory port map (
 		clock => clk,
-		writedata => open,
-		address => addr,
-		memwrite => open,
+		writedata => w_data,
+		address => addrint,
+		memwrite => w,
 		memread => memread,
 		readdata => instr,
 		waitrequest => waitrequest
