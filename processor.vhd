887999fd2cf8d202ca94bc63f2d5f6fd4a476bf7
James Madden <jamesbmadden@gmail.com>
2026-04-14 14:40:42 -0400

    add instrmem

--- processor.vhd
+++ processor.vhd
@@ -10,8 +10,12 @@ end processor;
 
 architecture behaviour of processor is
 
-  signal s_addr : std_logic_vector(31 downto 0) := (others => '0');
-  signal clk : std_logic;
+  -- global signals
+  signal clk : std_logic; -- temporary for debugging
+
+  -- instruction fetch stage signals
+  signal s_if_addr : std_logic_vector(31 downto 0) := (others => '0');
+  signal s_if_instr : std_logic_vector(31 downto 0) := (others => '0');
 
   -- declare components
   component pc is
@@ -24,6 +28,16 @@ architecture behaviour of processor is
     );
   end component;
 
+  component instrmem is
+    port (
+      clk : in std_logic;
+      w : in std_logic;
+      addr : in std_logic_vector(31 downto 0);
+      w_data : in std_logic_vector(31 downto 0);
+      instr : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
   CONSTANT clk_period : time := 1 ns;
 
 begin
@@ -33,9 +47,18 @@ begin
 		clk => clk,
 		stall => '0',
 		w => '0',
-    		w_addr => (others => '0'),
-    		addr => s_addr
+    w_addr => (others => '0'),
+    addr => s_if_addr
 	);
+
+  -- instrmem
+  i: instrmem port map (
+    clk => clk,
+    w => '0',
+    addr => s_if_addr,
+    w_data => (others => '0')
+    instr => s_if_instr
+  );
 
 clk_process : PROCESS
 BEGIN
