304c65e1db8807521f7a3486b4ac488751ef6742
sydneybrajer <sydney.brajer@gmail.com>
2026-04-14 18:17:14 -0400

    change writedata from 8 to 32 bits

--- datamem.vhd
+++ datamem.vhd
@@ -6,7 +6,7 @@ entity datamem is
     port (
         clk : in std_logic;
         addr : in std_logic_vector(31 downto 0);
-        writedata : in std_logic_vector(7 downto 0);
+        writedata : in std_logic_vector(31 downto 0);
         funct3 : in std_logic_vector(2 downto 0);
         memwrite : in std_logic;
         memread : in std_logic;
@@ -17,6 +17,24 @@ end datamem;
 
 architecture behaviour of datamem is
 
+    type state_type is (idle, load_wait, rmw_wait);
+    signal state : state_type := idle;
+
+    signal addrint : integer range 0 to 32767 := 0;
+    signal addr_reg : integer range 0 to 32767 := 0;
+    signal funct3_reg : std_logic_vector(2 downto 0) := (others => '0');
+    signal writedata_reg : std_logic_vector(31 downto 0) := (others => '0');
+
+    signal mem_addr : integer range 0 to 32767 := 0;
+    signal mem_writedata : std_logic_vector(31 downto 0) := (others => '0');
+    signal mem_readdata : std_logic_vector(31 downto 0);
+    signal memwrite_i : std_logic := '0';
+    signal memread_i : std_logic := '0';
+    signal mem_waitrequest : std_logic;
+
+    signal readdata_reg : std_logic_vector(31 downto 0) := (others => '0');
+    signal waitrequest_reg : std_logic := '0';
+
     component memory is
         generic(
             ram_size : integer := 32768;
@@ -25,217 +43,171 @@ architecture behaviour of datamem is
         );
         port (
             clock : in std_logic;
-            writedata : in std_logic_vector(7 downto 0);
+            writedata : in std_logic_vector(31 downto 0);
             address : in integer range 0 to ram_size-1;
             memwrite : in std_logic;
             memread : in std_logic;
-            readdata : out std_logic_vector(7 downto 0);
+            readdata : out std_logic_vector(31 downto 0);
             waitrequest : out std_logic
         );
     end component;
 
-    constant words : integer := 32768 / 4;
-
-    signal word_addr : integer range 0 to words-1;
-    signal byte_offset : std_logic_vector(1 downto 0);
-
-    signal lane0_in : std_logic_vector(7 downto 0);
-    signal lane1_in : std_logic_vector(7 downto 0);
-    signal lane2_in : std_logic_vector(7 downto 0);
-    signal lane3_in : std_logic_vector(7 downto 0);
-
-    signal lane0_out : std_logic_vector(7 downto 0);
-    signal lane1_out : std_logic_vector(7 downto 0);
-    signal lane2_out : std_logic_vector(7 downto 0);
-    signal lane3_out : std_logic_vector(7 downto 0);
-
-    signal wr0 : std_logic;
-    signal wr1 : std_logic;
-    signal wr2 : std_logic;
-    signal wr3 : std_logic;
-
-    signal wait0 : std_logic;
-    signal wait1 : std_logic;
-    signal wait2 : std_logic;
-    signal wait3 : std_logic;
-
-begin
-    word_addr <= to_integer(unsigned(addr(14 downto 2)));
-    byte_offset <= addr(1 downto 0);
-
-    process(memwrite, funct3, byte_offset, writedata)
+    function swap_word(x : std_logic_vector(31 downto 0)) return std_logic_vector is
+        variable y : std_logic_vector(31 downto 0);
     begin
-        wr0 <= '0';
-        wr1 <= '0';
-        wr2 <= '0';
-        wr3 <= '0';
+        y := x(7 downto 0) & x(15 downto 8) & x(23 downto 16) & x(31 downto 24);
+        return y;
+    end function;
 
-        lane0_in <= (others => '0');
-        lane1_in <= (others => '0');
-        lane2_in <= (others => '0');
-        lane3_in <= (others => '0');
-
-        case funct3 is
-            when "000" =>
-                case byte_offset is
-                    when "00" =>
-                        wr0 <= memwrite;
-                        lane0_in <= writedata(7 downto 0);
-                    when "01" =>
-                        wr1 <= memwrite;
-                        lane1_in <= writedata(7 downto 0);
-                    when "10" =>
-                        wr2 <= memwrite;
-                        lane2_in <= writedata(7 downto 0);
-                    when others =>
-                        wr3 <= memwrite;
-                        lane3_in <= writedata(7 downto 0);
-                end case;
-
-            when "001" =>
-                if byte_offset(1) = '0' then
-                    wr0 <= memwrite;
-                    wr1 <= memwrite;
-                    lane0_in <= writedata(7 downto 0);
-                    lane1_in <= writedata(15 downto 8);
-                else
-                    wr2 <= memwrite;
-                    wr3 <= memwrite;
-                    lane2_in <= writedata(7 downto 0);
-                    lane3_in <= writedata(15 downto 8);
-                end if;
-
-            when "010" =>
-                wr0 <= memwrite;
-                wr1 <= memwrite;
-                wr2 <= memwrite;
-                wr3 <= memwrite;
-                lane0_in <= writedata(7 downto 0);
-                lane1_in <= writedata(15 downto 8);
-                lane2_in <= writedata(23 downto 16);
-                lane3_in <= writedata(31 downto 24);
-
-            when others =>
-                null;
-        end case;
-    end process;
-
-    mem0 : memory
-        generic map(
-            ram_size => words
-        )
-        port map(
-            clock => clk,
-            writedata => lane0_in,
-            address => word_addr,
-            memwrite => wr0,
-            memread => memread,
-            readdata => lane0_out,
-            waitrequest => wait0
-        );
-
-    mem1 : memory
-        generic map(
-            ram_size => words
-        )
-        port map(
-            clock => clk,
-            writedata => lane1_in,
-            address => word_addr,
-            memwrite => wr1,
-            memread => memread,
-            readdata => lane1_out,
-            waitrequest => wait1
-        );
-
-    mem2 : memory
-        generic map(
-            ram_size => words
-        )
-        port map(
-            clock => clk,
-            writedata => lane2_in,
-            address => word_addr,
-            memwrite => wr2,
-            memread => memread,
-            readdata => lane2_out,
-            waitrequest => wait2
-        );
-
-    mem3 : memory
-        generic map(
-            ram_size => words
-        )
-        port map(
-            clock => clk,
-            writedata => lane3_in,
-            address => word_addr,
-            memwrite => wr3,
-            memread => memread,
-            readdata => lane3_out,
-            waitrequest => wait3
-        );
-
-    process(memread, funct3, byte_offset, lane0_out, lane1_out, lane2_out, lane3_out)
+    function format_load(f3 : std_logic_vector(2 downto 0); x : std_logic_vector(31 downto 0)) return std_logic_vector is
+        variable y : std_logic_vector(31 downto 0);
         variable b : std_logic_vector(7 downto 0);
         variable h : std_logic_vector(15 downto 0);
-        variable w : std_logic_vector(31 downto 0);
-        variable outv : std_logic_vector(31 downto 0);
     begin
-        outv := (others => '0');
+        y := (others => '0');
+        b := x(31 downto 24);
+        h := x(23 downto 16) & x(31 downto 24);
 
-        w := lane3_out & lane2_out & lane1_out & lane0_out;
+        case f3 is
+            when "000" =>
+                y(7 downto 0) := b;
+                if b(7) = '1' then
+                    y(31 downto 8) := (others => '1');
+                else
+                    y(31 downto 8) := (others => '0');
+                end if;
 
-        case byte_offset is
-            when "00" => b := lane0_out;
-            when "01" => b := lane1_out;
-            when "10" => b := lane2_out;
-            when others => b := lane3_out;
+            when "100" =>
+                y(7 downto 0) := b;
+                y(31 downto 8) := (others => '0');
+
+            when "001" =>
+                y(15 downto 0) := h;
+                if h(15) = '1' then
+                    y(31 downto 16) := (others => '1');
+                else
+                    y(31 downto 16) := (others => '0');
+                end if;
+
+            when "101" =>
+                y(15 downto 0) := h;
+                y(31 downto 16) := (others => '0');
+
+            when "010" =>
+                y := swap_word(x);
+
+            when others =>
+                y := (others => '0');
         end case;
 
-        if byte_offset(1) = '0' then
-            h := lane1_out & lane0_out;
-        else
-            h := lane3_out & lane2_out;
-        end if;
+        return y;
+    end function;
 
-        if memread = '1' then
-            case funct3 is
-                when "000" =>
-                    outv(7 downto 0) := b;
-                    if b(7) = '1' then
-                        outv(31 downto 8) := (others => '1');
+    function rmw_word(f3 : std_logic_vector(2 downto 0); newdata : std_logic_vector(31 downto 0); oldword : std_logic_vector(31 downto 0)) return std_logic_vector is
+        variable y : std_logic_vector(31 downto 0);
+    begin
+        y := oldword;
+
+        case f3 is
+            when "000" =>
+                y := newdata(7 downto 0) & oldword(23 downto 0);
+
+            when "001" =>
+                y := newdata(7 downto 0) & newdata(15 downto 8) & oldword(15 downto 0);
+
+            when "010" =>
+                y := swap_word(newdata);
+
+            when others =>
+                y := oldword;
+        end case;
+
+        return y;
+    end function;
+
+begin
+
+    addrint <= to_integer(unsigned(addr(14 downto 0)));
+
+    mem_inst : memory
+        port map(
+            clock => clk,
+            writedata => mem_writedata,
+            address => mem_addr,
+            memwrite => memwrite_i,
+            memread => memread_i,
+            readdata => mem_readdata,
+            waitrequest => mem_waitrequest
+        );
+
+    process(state, memread, memwrite, addrint, addr_reg, funct3, funct3_reg, writedata, writedata_reg, mem_readdata)
+    begin
+        memread_i <= '0';
+        memwrite_i <= '0';
+        mem_addr <= addrint;
+        mem_writedata <= (others => '0');
+        waitrequest_reg <= '0';
+
+        case state is
+            when idle =>
+                if memread = '1' then
+                    memread_i <= '1';
+                    mem_addr <= addrint;
+                    waitrequest_reg <= '1';
+                elsif memwrite = '1' then
+                    waitrequest_reg <= '1';
+                    if funct3 = "010" then
+                        memwrite_i <= '1';
+                        mem_addr <= addrint;
+                        mem_writedata <= swap_word(writedata);
                     else
-                        outv(31 downto 8) := (others => '0');
+                        memread_i <= '1';
+                        mem_addr <= addrint;
                     end if;
+                end if;
 
-                when "100" =>
-                    outv(7 downto 0) := b;
-                    outv(31 downto 8) := (others => '0');
+            when load_wait =>
+                waitrequest_reg <= '1';
 
-                when "001" =>
-                    outv(15 downto 0) := h;
-                    if h(15) = '1' then
-                        outv(31 downto 16) := (others => '1');
-                    else
-                        outv(31 downto 16) := (others => '0');
-                    end if;
-
-                when "101" =>
-                    outv(15 downto 0) := h;
-                    outv(31 downto 16) := (others => '0');
-
-                when "010" =>
-                    outv := w;
-
-                when others =>
-                    outv := (others => '0');
-            end case;
-        end if;
-
-        readdata <= outv;
+            when rmw_wait =>
+                waitrequest_reg <= '1';
+                memwrite_i <= '1';
+                mem_addr <= addr_reg;
+                mem_writedata <= rmw_word(funct3_reg, writedata_reg, mem_readdata);
+        end case;
     end process;
 
-    waitrequest <= wait0 and wait1 and wait2 and wait3;
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            case state is
+                when idle =>
+                    if memread = '1' then
+                        addr_reg <= addrint;
+                        funct3_reg <= funct3;
+                        state <= load_wait;
+                    elsif memwrite = '1' then
+                        addr_reg <= addrint;
+                        funct3_reg <= funct3;
+                        writedata_reg <= writedata;
+                        if funct3 = "010" then
+                            state <= idle;
+                        else
+                            state <= rmw_wait;
+                        end if;
+                    end if;
+
+                when load_wait =>
+                    readdata_reg <= format_load(funct3_reg, mem_readdata);
+                    state <= idle;
+
+                when rmw_wait =>
+                    state <= idle;
+            end case;
+        end if;
+    end process;
+
+    readdata <= readdata_reg;
+    waitrequest <= waitrequest_reg;
 
 end behaviour;
