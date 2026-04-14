6698533ed73871bbcb2fb55ef450348a01e676ce
James Madden <jamesbmadden@gmail.com>
2026-04-14 14:25:42 -0400

    make processor a temporary testbench

--- processor.vhd
+++ processor.vhd
@@ -3,29 +3,46 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity processor is
-  port (
-    -- tbd
-  );
+  -- port (
+    -- clk : in std_logic
+  -- );
 end processor;
 
-architecture behaviour of pipeline is
+architecture behaviour of processor is
 
-  -- declare the memory component
-  component memory is
-    port ();
+  signal s_addr : std_logic_vector(31 downto 0) := (others => '0');
+  signal clk : std_logic;
+
+  -- declare components
+  component pc is
+    port (
+      clk : in std_logic;
+      stall : in std_logic;
+      w : in std_logic;
+      w_addr : in std_logic_vector(31 downto 0);
+      addr : out std_logic_vector(31 downto 0)
+    );
   end component;
 
-begin
-  -- create instr memory
-  instr: memory 
-    port map (
-      
-    );
+  CONSTANT clk_period : time := 1 ns;
 
-  -- create data memory
-  data: memory 
-    port map (
-      
-    );
+begin
+	
+  -- connect to the appropriate ports of a memory instance
+	p: pc port map (
+		clk => clk,
+		stall => '0',
+		w => '0',
+    		w_addr => (others => '0'),
+    		addr => s_addr
+	);
+
+clk_process : PROCESS
+BEGIN
+	clk <= '0';
+	WAIT FOR clk_period/2;
+	clk <= '1';
+	WAIT FOR clk_period/2;
+END PROCESS;
 
 end behaviour;
